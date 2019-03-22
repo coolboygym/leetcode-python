7acdb81a77da0bacfecfff10a4c97f33ddd30c4e
gaoyimei <gym2015@qq.com>
2019-03-22 09:22:31 +0800

    feat: 丑数

---
+++ 263_丑数.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 0:
+            return False
+        while num % 2 == 0:
+            num = num // 2
+        while num % 3 == 0:
+            num = num // 3
+        while num % 5 == 0:
+            num = num // 5
+
+        return num == 1
+
