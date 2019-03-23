386491465448f72cf90e47b9619b0cfb11a1ae34
gaoyimei <gym2015@qq.com>
2019-03-23 17:39:14 +0800

    feat: 前K个高频单词

---
+++ 692_前K个高频单词.py
@@ -0,0 +1,13 @@
+import collections
+import heapq
+
+
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+
+        return [x[1] for x in heapq.nsmallest(k, [(v, k) for k, v in collections.Counter(words).items()], key=lambda a: (-a[0], a[1]))]
---
+++ 973.py
@@ -0,0 +1,12 @@
+import collections
+import heapq
+
+
+class Solution(object):
+    def kClosest(self, points, K):
+        """
+        :type points: List[List[int]]
+        :type K: int
+        :rtype: List[List[int]]
+        """
+        return [x[1] for x in heapq.nsmallest(K, [(p[0] ** 2 + p[1] ** 2, p) for p in points])]
