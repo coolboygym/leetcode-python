e52f7b384366f6aedcbc5c5a2a445483048439d7
gaoyimei <gym2015@qq.com>
2019-04-12 09:02:14 +0800

    feat: 一和零->动态规划、背包问题

---
+++ 474_一和零.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def findMaxForm(self, strs, m, n):
+        """
+        :type strs: List[str]
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        size = len(strs)
+        if size == 0:
+            return 0
+
+        # 初始化dp数组
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+
+        # 遍历每一个字符串
+        for s in strs:
+            zeros = 0
+            ones = 0
+            for char in s:
+                if char == '0':
+                    zeros += 1
+                else:
+                    ones += 1
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    # 两种情况取最大值 不使用该字符串和使用该字符串
+                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
+
+        return dp[m][n]
