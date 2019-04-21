76f3bfa632b7a617f6b250f7aecb8a97da586be5
gaoyimei <gym2015@qq.com>
2019-04-21 14:22:15 +0800

    feat: 最佳买卖股票时机含冷冻期->动态规划

---
+++ 309_最佳买卖股票时机含冷冻期.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        """
+        sell[i]表示截至第i天，最后一个操作是卖时的最大收益；
+        buy[i]表示截至第i天，最后一个操作是买时的最大收益；
+        cool[i]表示截至第i天，最后一个操作是冷冻期时的最大收益；
+        """
+        n = len(prices)
+        if n == 0:
+            return 0
+        sell = [0 for _ in range(n)]
+        buy = [0 for _ in range(n)]
+        cool = [0 for _ in range(n)]
+        buy[0] = -prices[0]
+        for i in range(1, n):
+            sell[i] = max(buy[i - 1] + prices[i], sell[i - 1])
+            buy[i] = max(cool[i - 1] - prices[i], buy[i - 1])
+            cool[i] = sell[i - 1]
+        return sell[-1]
